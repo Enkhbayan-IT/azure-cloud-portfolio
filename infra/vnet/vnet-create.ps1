448f258369e35d6cd8bf1904df35edb6286bd288
Enkhbayan Altangerel <enkhbayan0813@gmail.com>
2026-04-23 18:26:52 +0200

    Add script to create Azure Virtual Network

---
+++ infra/vnet/vnet-create.ps1
@@ -0,0 +1,10 @@
+$ResourceGroupName ="rg-az-swc-dev"
+$Location          ="swedencentral"
+$VNetName          ="vnet-swc-dev-01"
+$AddressSpace           ="10.0.0.0/16"
+
+New-AzVirtualNetwork `
+                    -Name $VNetName `
+                    -ResourceGroupName $ResourceGroupName `
+                    -Location $Location `
+                    -AddressPrefix $AddressSpace
