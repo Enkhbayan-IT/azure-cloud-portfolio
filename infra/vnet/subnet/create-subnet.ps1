2c21a0da093d069ed6cb9f8e489b16d53b969ad6
Enkhbayan Altangerel <enkhbayan0813@gmail.com>
2026-04-27 20:31:56 +0200

    Update subnet variables for new development environment

--- infra/vnet/subnet/create-subnet.ps1
+++ infra/vnet/subnet/create-subnet.ps1
@@ -1,8 +1,8 @@
-#Define variables
-$ResourceGroupName   ="rg-az-swc-dev"
-$VNetName            ="vnet-swc-dev-01"
-$SubnetName          ="snet-swc-dev-web-01"
-$SubnetAddressPrefix ="10.0.1.0/24"
+# Variables
+$ResourceGroupName   ="rg-az-neu-dev"
+$VNetName            ="vnet-neu-dev-01"
+$SubnetName          ="snet-neu-dev-web-01"
+$SubnetAddressPrefix ="10.0.0.0/24"
 
 # Get existing VNet
 $VNet = Get-AzVirtualNetwork `
